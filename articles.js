f3f9928d925a809c4c6ab62f7c7a081bf2cb4852
Praise Caleb <praisecaleb11@gmail.com>
2025-10-18 11:19:21 +0100

    completed first view of home page

---
+++ articles.js
@@ -0,0 +1,58 @@
+var showArtRich = document.getElementById("show-art").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box-rich").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var cancelArtBoxRich = document.querySelector(".cancel-art-box-rich").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box-rich").style.display="none";
+    let art = document.querySelector(".articles1").style.display="flex";
+    let art1 = document.querySelector(".articles2").style.display="flex";
+    let artr = document.querySelector(".article-return").style.display="flex";
+});
+
+var showArt1 = document.getElementById("show-art1").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var showArt2 = document.getElementById("show-art2").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var showArt3 = document.getElementById("show-art3").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var showArt4 = document.getElementById("show-art4").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var showArt5 = document.getElementById("show-art5").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="block";
+    let art = document.querySelector(".articles1").style.display="none";
+    let art1 = document.querySelector(".articles2").style.display="none";
+    let artr = document.querySelector(".article-return").style.display="none";
+});
+
+var articleHeadTxt = document.querySelector(".article-head-txt").addEventListener('click', ()=>{
+    let show = document.querySelector(".art-box").style.display="none";
+    let art = document.querySelector(".articles1").style.display="block";
+    let art1 = document.querySelector(".articles2").style.display="block";
+    let artr = document.querySelector(".article-return").style.display="block";
+});
+function artic(){
+    
+}
